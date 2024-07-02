7338e3a92c2e7fcca8a10705f30caa21bcf6ad19
XxXNe0XxX <jm82467913@gmail.com>
2024-07-02 09:28:09 -0500

    starting deploy

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthProvider.jsx";
 import { ModalProvider } from "./context/ModalProvider.jsx";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+if (process.env.NODE_ENV === "production") disableReactDevTools();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
