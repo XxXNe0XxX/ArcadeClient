9832ad1a0c82a20718da81022e19583540a52133
XxXNe0XxX <jm82467913@gmail.com>
2024-07-02 10:05:16 -0500

    fixed vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,31 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import svgrPlugin from "vite-plugin-svgr";
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [svgrPlugin(), react()],
+//   // server: {
+//   //   host: "0.0.0.0",
+//   //   proxy: {
+//   //     "/api": {
+//   //       target: "http://192.168.1.27:3000", // Backend server address
+//   //       changeOrigin: true,
+//   //       rewrite: (path) => path.replace(/^\/api/, ""),
+//   //     },
+//   //   },
+//   // },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgrPlugin from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgrPlugin(), react()],
+  base: "/", // Ensure this is correctly set for your deployment
+  plugins: [react(), svgrPlugin()],
+  build: {
+    outDir: "dist",
+  },
   // server: {
   //   host: "0.0.0.0",
   //   proxy: {
